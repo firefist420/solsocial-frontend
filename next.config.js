a62bcacd424f6686a06a90112d53ceb412f7aa9a
firefist420 <bearded_dad_69@outlook.com>
2025-04-13 22:19:47 -0400

    Update Dependincies

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['assets.dexscreener.com'],
+    domains: ['assets.dexscreener.com', 'arweave.net', 'nftstorage.link', 'ipfs.io'],
   },
   env: {
     NEXT_PUBLIC_HCAPTCHA_SITEKEY: process.env.HCAPTCHA_SITEKEY,
     NEXT_PUBLIC_SOLANA_RPC_URL: process.env.SOLANA_RPC_URL,
     NEXT_PUBLIC_HELIUS_API_KEY: process.env.HELIUS_API_KEY,
-    MONGODB_URI: process.env.MONGODB_URI,
-    MONGODB_DB: process.env.MONGODB_DB
+    NEXT_PUBLIC_MONGODB_URI: process.env.MONGODB_URI,
+    NEXT_PUBLIC_MONGODB_DB: process.env.MONGODB_DB,
+    NEXT_PUBLIC_DEXSCREENER_API: process.env.DEXSCREENER_API,
+    JWT_SECRET: process.env.JWT_SECRET
   },
   webpack: (config) => {
     config.resolve.fallback = {
@@ -16,8 +18,15 @@ module.exports = {
       net: false,
       tls: false,
       bufferutil: false,
-      'utf-8-validate': false
+      'utf-8-validate': false,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      path: require.resolve('path-browserify')
     };
     return config;
+  },
+  experimental: {
+    esmExternals: 'loose',
+    serverComponentsExternalPackages: ['mongoose']
   }
 };
