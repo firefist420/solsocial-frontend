1fb5d8b3469cde939bbec7f1c6ed3b0bc06d841e
firefist420 <bearded_dad_69@outlook.com>
2025-04-24 16:20:09 -0400

    FIx AgAin

--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,20 @@ import { useMemo } from 'react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { 
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  BackpackWalletAdapter,
-  GlowWalletAdapter
-} from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { clusterApiUrl } from '@solana/web3.js';
 
 function MyApp({ Component, pageProps }) {
   const network = WalletAdapterNetwork.Mainnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   
-  const wallets = useMemo(() => [
-    new PhantomWalletAdapter(),
-    new SolflareWalletAdapter(),
-    new BackpackWalletAdapter(),
-    new GlowWalletAdapter()
-  ], [network]);
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter()
+    ],
+    [network]
+  );
 
   return (
     <ConnectionProvider endpoint={endpoint}>
