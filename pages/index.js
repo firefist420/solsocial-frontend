70d3812e74e5dcba9bbc69ee0ccfaeed0fb660e0
firefist420 <bearded_dad_69@outlook.com>
2025-04-24 17:03:00 -0400

    Phantom

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 const PhantomLoginButton = () => {
-  const { connect, connected, publicKey } = useWallet();
+  const { connect, connected, publicKey, select } = useWallet();
   const router = useRouter();
   const [isConnecting, setIsConnecting] = useState(false);
 
@@ -17,6 +17,7 @@ const PhantomLoginButton = () => {
   const handleConnect = async () => {
     try {
       setIsConnecting(true);
+      select('Phantom');
       await connect();
     } catch (error) {
       console.error(error);
@@ -33,7 +34,10 @@ const PhantomLoginButton = () => {
     >
       {isConnecting ? 'Connecting...' : (
         <>
-          <img src="/phantom-icon.png" alt="Phantom" className="w-6 h-6 mr-2"/>
+          <svg className="w-6 h-6 mr-2" viewBox="0 0 24 24" fill="none">
+            <path d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24Z" fill="#AB9FF2"/>
+            <path d="M13.6555 6H17.5L16.5 7.5H13.6555C12.7503 7.5 12 8.2503 12 9.1555V14.8445C12 15.7497 12.7503 16.5 13.6555 16.5H16.5L17.5 18H13.6555C11.6459 18 10 16.3541 10 14.3445V9.6555C10 7.64594 11.6459 6 13.6555 6Z" fill="white"/>
+          </svg>
           Connect Phantom Wallet
         </>
       )}
