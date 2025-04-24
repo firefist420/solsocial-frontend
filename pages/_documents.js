0e1f298248b7d3daec2960c5af13506530e9bcff
firefist420 <bearded_dad_69@outlook.com>
2025-04-23 20:26:18 -0400

    documents

--- pages/_documents.js
+++ pages/_documents.js
@@ -5,22 +5,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
-<<<<<<< HEAD
         <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' https://js.hcaptcha.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'" />
         <script 
           src="https://js.hcaptcha.com/1/api.js" 
-          async defer
-        ></script>
-      </Head>
-      <body>
-=======
-        <script 
-          src="https://js.hcaptcha.com/1/api.js" 
-          async defer
+          async 
+          defer
         />
       </Head>
-      <body className="bg-gray-900 text-white">
->>>>>>> c145543 (Your commit message)
+      <body>
         <Main />
         <NextScript />
       </body>
